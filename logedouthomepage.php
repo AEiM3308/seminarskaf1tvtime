143f2f311afc8b9e748ca2e9577ac3c5214d4fce
Ana Jagarinec <163204906+AEiM3308@users.noreply.github.com>
2024-03-14 22:36:15 +0100

    Added logout

---
+++ logedouthomepage.php
@@ -0,0 +1,5 @@
+<div class="flex flex-col gap-4">
+    <h2 class="text-2xl">Not logged in!</h2>
+    <a href="/seminarska/auth/login.php">Log in</a>
+    <a href="/seminarska/auth/register.php">Register</a>
+</div>
