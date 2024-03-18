70183e0c406558055bcd08265e6ab510db4ddeaf
Ana Jagarinec <163204906+AEiM3308@users.noreply.github.com>
2024-03-19 00:49:40 +0100

    fix constraints

--- database.sql
+++ database.sql
@@ -2,37 +2,39 @@ CREATE DATABASE seminarska;
 USE seminarska;
 
 CREATE TABLE users (
-    id SERIAL PRIMARY KEY NOT NULL,
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
     password_hash VARCHAR(255) NOT NULL,
     username VARCHAR(255) UNIQUE NOT NULL
 );
 
 CREATE TABLE seasons(
-    id SERIAL PRIMARY KEY NOT NULL,
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
     year INTEGER NOT NULL
 );
 
 CREATE TABLE races(
-    id SERIAL PRIMARY KEY NOT NULL,
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
     date DATE NOT NULL,
     name VARCHAR(255) NOT NULL,
     location VARCHAR(255) NOT NULL,
-    cover VARCHAR(255) NOT NULL,
-    winner_driver VARCHAR(255),
-    winner_team VARCHAR(255),
     season_id INTEGER,
-    CONSTRAINT "FK_season" FOREIGN KEY("season_id") REFERENCES "seasons"("id")
+    CONSTRAINT FK_season FOREIGN KEY(season_id) REFERENCES seasons(id)
 );
 
-CREATE TABLE images(
-    id SERIAL PRIMARY KEY NOT NULL,
+CREATE TABLE images (
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
     path VARCHAR(255) NOT NULL,
-    CONSTRAINT "FK_race" FOREIGN KEY("race_id") REFERENCES "race"("id")
+    season_id INT,
+    CONSTRAINT FK_image_season_id FOREIGN KEY (season_id) REFERENCES seasons (id)
 );
+
 
 CREATE TABLE watched(
-    id SERIAL PRIMARY KEY NOT NULL,
-    CONSTRAINT "FK_race" FOREIGN KEY("race_id") REFERENCES "race"("id"),
-    CONSTRAINT "FK_user" FOREIGN KEY("user_id") REFERENCES "user"("id")
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    CONSTRAINT FK_race FOREIGN KEY(race_id) REFERENCES race(id),
+    CONSTRAINT FK_user FOREIGN KEY(user_id) REFERENCES user(id)
 );
+
+ALTER TABLE seasons 
+ADD UNIQUE (year); 
