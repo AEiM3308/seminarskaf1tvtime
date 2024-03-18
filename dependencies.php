460694a4bf86df65f0b340c16c04c9a2396785e2
Ana Jagarinec <163204906+AEiM3308@users.noreply.github.com>
2024-03-19 00:49:54 +0100

    added global styling

--- dependencies.php
+++ dependencies.php
@@ -3,3 +3,21 @@ include_once("db.php");
 session_start();
 ?>
 <script src="https://cdn.tailwindcss.com"></script>
+<style type="text/tailwindcss">
+  @layer base {
+    button, a.button, input[type="submit"] {
+      @apply cursor-pointer border-none px-4 py-2 rounded-full text-white !bg-red-600 hover:bg-red-700 text-white font-semibold
+    }
+
+    input {
+      @apply px-4 p-2 rounded-full border bg-transparent border-neutral-400 hover:border-red-600 focus:border-red-600  
+    }
+
+    body {
+      @apply bg-[#212121] text-white p-4;
+    }
+
+
+  }
+
+  </style>
